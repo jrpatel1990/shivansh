dc81f97bcc8c8a8ab23dd8d54dde8855a90bde10
jrpatel1990 <130759064+jrpatel1990@users.noreply.github.com>
2023-04-23 23:12:08 +0530

    push

---
+++ lesson1.c
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Example of Css</title>
+     <style>
+          h1
+          {
+               font-weight:100;
+               color: rgb(255, 255, 255);
+               background-color: rgb(0, 0, 0);
+          }
+          p
+          {
+               font-size: 1.5rem;
+               background-color:rgb(245, 201, 201);;
+               /* color: white; */
+               /* background-color: rgb(245, 201, 201); */
+          }
+     </style>
+</head>
+<body>
+     <h1 align="center">
+          This is page heading 
+     </h1>
+     <hr>
+     <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laborum minima repudiandae dolores officiis, recusandae consectetur, quam officia iure tempore et blanditiis illum. Dolor suscipit possimus officiis corrupti deleniti magni.
+     </p>
+     <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quidem architecto eos laborum voluptates minima. Magnam illum quasi eligendi rerum distinctio officia consectetur enim dolor et facilis quaerat, voluptas sequi!
+     </p>
+     <h1>
+          this is heading 2 
+     </h1>
+</body>
+</html>
+Footer
